f791225357043b956d9d2ddf798088af1ce762ce
Ray-de-light <artista.del.alma@gmail.com>
2023-10-07 14:19:48 -0500

    Did some cleanup
    js
    small errors

--- scripts.js
+++ scripts.js
@@ -1,80 +1,79 @@
-// JS code to close mobile menu with escape key //
+// JS code to close mobile menu with escape key //document.querySe
+
+/* This is placed on the HTML page in the <head> under the links
+    <script>
+        // script for sticky menu
+        var prevScrollpos = window.pageYOffset;
+        window.onscroll = function () {
+            var currentScrollPos = window.pageYOffset;
+            if (prevScrollpos > currentScrollPos) {
+                document.querySelector('#nav').style.top = "0";
+            } else {
+                document.querySelector('#nav').style.top = "-70px";
+            }
+            prevScrollpos = currentScrollPos;
+        } 
+    </script> */
 
 // Select elements
-const menu = document.querySelector('.menu');
-const menuButtonContainer = document.querySelector('.menu-button-container');
-const menuToggle = document.querySelector('#menu-toggle');
-const navLinks = document.querySelectorAll('#nav a');
-const menuItems = document.querySelectorAll('.menu > li');
+const menu = document.querySelector(".menu");
+const menuButtonContainer = document.querySelector(".menu-button-container");
+const menuToggle = document.querySelector("#menu-toggle");
+const navLinks = document.querySelectorAll("#nav a");
+const menuItems = document.querySelectorAll(".menu > li");
 
 // Add event listeners
-menuToggle.addEventListener('click', toggleMenu);
-document.addEventListener('keydown', closeMenu);
+menuToggle.addEventListener("click", toggleMenu);
+document.addEventListener("keydown", closeMenu);
 
 // Functions
 function toggleMenu() {
-  menu.classList.toggle('show-menu');
-  menuButtonContainer.classList.toggle('close');
+  menu.classList.toggle("show-menu");
+  menuButtonContainer.classList.toggle("close");
 }
 
 function closeMenu(event) {
-  if (event.key === 'Escape' && menu.classList.contains('show-menu')) {
-    menu.classList.remove('show-menu');
-    menuButtonContainer.classList.remove('close');
+  if (event.key === "Escape" && menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    menuButtonContainer.classList.remove("close");
   }
 }
 
 // Close menu when a link is clicked
 navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    menu.classList.remove('show-menu');
-    menuButtonContainer.classList.remove('close');
+  link.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+    menuButtonContainer.classList.remove("close");
   });
 });
 
 // Close menu when a menu item is clicked
 menuItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    menu.classList.remove('show-menu');
-    menuButtonContainer.classList.remove('close');
+  item.addEventListener("click", () => {
+    menu.classList.remove("show-menu");
+    menuButtonContainer.classList.remove("close");
   });
 });
 
-    const body = document.body;
+const body = document.body;
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-	const currentScroll = window.pageYOffset;
-	if (currentScroll <= 0) {
-		body.classList.remove("scroll-up");
-		return;
-	}
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
 
-	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-		body.classList.remove("scroll-up");
-		body.classList.add("scroll-down");
-	} else if (
-		currentScroll < lastScroll &&
-		body.classList.contains("scroll-down")
-	) {
-		body.classList.remove("scroll-down");
-		body.classList.add("scroll-up");
-	}
-	lastScroll = currentScroll;
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
 });
-
-// This is for sticky header on the HTML page
-    // <script>
-    //     var prevScrollpos = window.pageYOffset;
-
-    //     window.onscroll = function () {
-    //         var currentScrollPos = window.pageYOffset;
-
-    //         if (prevScrollpos > currentScrollPos) {
-    //             document.getElementById("#nav").classList.remove("hide");
-    //         } else {
-    //             document.getElementById("#nav").classList.add("hide");
-    //         }
-
-    //         prevScrollpos = currentScrollPos;
-    //     }
